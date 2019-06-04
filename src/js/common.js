fe6c8edc7363caab5af6db8b0f4e0f51a45edb2f
Shamil <maashevshamil@gmail.com>
2019-06-05 01:03:24 +0500

    add dropdown, date dropdown, masked text field, filter date dropdown and subscription text field blocks

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,4 @@
-var elem = document.getElementById('test');
+var elem = document.querySelector('div.dropdown_vis__input_svg');
 var elems = document.getElementById('test1');
 elem.onclick = elems.style.display = 'none';;
 function func() {
@@ -10,6 +10,17 @@ function func() {
   }
   elem.onclick = func;
 
+  var elem1 = document.querySelector('div.dropdown_bedroom__input_svg');
+  var elems1 = document.querySelector('div.dropdown_bedroom_content');
+  elem1.onclick = elems1.style.display = 'none';;
+  function func1() {
+      if (elems1.style.display == 'none') {
+        elems1.style.display = 'flex';
+      } else {
+        elems1.style.display = 'none'
+      }
+    }
+    elem1.onclick = func1;
 
 
 // Инициализация
