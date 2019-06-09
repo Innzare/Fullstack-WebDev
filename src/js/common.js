74ba107067bdea5bdbdea2f85267e9a0e88c278c
Shamil <maashevshamil@gmail.com>
2019-06-09 18:54:18 +0500

    add comfort and checkbox_buttons blocks

--- src/js/common.js
+++ src/js/common.js
@@ -49,5 +49,23 @@ $('#my-element').datepicker([options])
 // Доступ к экземпляру объекта
 $('#my-element').data('datepicker')
 
+var $start = $('#start'),
+    $end = $('#end');
+
+$start.datepicker({
+    onSelect: function (fd, date) {
+        $end.data('datepicker')
+                .update('minDate', date);
+
+        $end.focus();
+    }
+})
+
+$end.datepicker({
+    onSelect: function (fd, date) {
+        $start.data('datepicker')
+                .update('maxDate', date)
+    }
+})
 
 
