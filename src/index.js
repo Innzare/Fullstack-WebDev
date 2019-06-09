8251240f67a2906855975d671e156fdd4365cef0
Shamil <maashevshamil@gmail.com>
2019-06-09 06:52:26 +0500

    stylize air datepicker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import 'jquery'
 import 'popper.js'
 import './js/common.js'
+import './../air/air-datepicker/dist/js/datepicker.js'
+import './../air/air-datepicker/dist/css/datepicker.css'
 import './css/main.css'
 import './scss/style.scss' 
 
